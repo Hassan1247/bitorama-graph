d17f7e01a51fd1249475bbe610f82cecb27247bd
Hassan-M <hassan.ma1245@gmail.com>
2021-10-05 16:44:14 +0330

    add models

--- weblog/bitorama/admin.py
+++ weblog/bitorama/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'date_created']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'text', 'username', 'verified', 'date_created']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text', 'date_created']
+
+
+@admin.register(Suggestion)
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ['username', 'subject', 'email', 'date_created']
--- weblog/bitorama/models.py
+++ weblog/bitorama/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=512)
+    picture = models.FileField()
+    post = models.URLField(max_length=256)
+    author = models.CharField(max_length=256, default='Hassan Moosaabadi')
+    categories = models.ManyToManyField('Category', blank=True)
+    number_of_views = models.PositiveIntegerField(default=0)
+    number_of_likes = models.PositiveIntegerField(default=0)
+    number_of_comments = models.PositiveIntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    username = models.CharField(max_length=256)
+    verified = models.BooleanField(default=False)
+    number_of_views = models.PositiveIntegerField(default=0)
+    number_of_likes = models.PositiveIntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.text
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Info(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Suggestion(models.Model):
+    username = models.CharField(max_length=256)
+    subject = models.CharField(max_length=256)
+    text = models.TextField()
+    email = models.EmailField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
