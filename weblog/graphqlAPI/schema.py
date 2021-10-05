5b2683d654955da50a7dfb3eba3f57b9979d5ec3
Hassan-M <hassan.ma1245@gmail.com>
2021-10-05 18:24:01 +0330

    add queries for other models

--- weblog/graphqlAPI/schema.py
+++ weblog/graphqlAPI/schema.py
@@ -55,10 +55,25 @@ class InfoType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     categories = graphene.List(CategoryType)
+    posts = graphene.List(PostType)
+    comments = graphene.List(CommentType)
+    infos = graphene.List(InfoType)
 
     def resolve_categories(root, info, **kwargs):
         # Querying a list
         return Category.objects.all()
+
+    def resolve_posts(root, info, **kwargs):
+        # Querying a list
+        return Post.objects.all()
+
+    def resolve_comments(root, info, **kwargs):
+        # Querying a list
+        return Comment.objects.all()
+
+    def resolve_infos(root, info, **kwargs):
+        # Querying a list
+        return Info.objects.all()
 
 
 schema = graphene.Schema(query=Query)
