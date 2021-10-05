780c9dec0e55918ad7e583a2c3604d3060335a68
Hassan-M <hassan.ma1245@gmail.com>
2021-10-05 18:43:00 +0330

    csrf token exempt

--- weblog/weblog/urls.py
+++ weblog/weblog/urls.py
@@ -19,11 +19,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from graphene_django.views import GraphQLView
 from graphqlAPI.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
