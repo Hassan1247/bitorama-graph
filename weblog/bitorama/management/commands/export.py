30950df79604d8cf4b5c377f15bdcc6f6b2e459d
Hassan-M <hassan.ma1245@gmail.com>
2021-10-07 12:56:56 +0330

    export posts

---
+++ weblog/bitorama/management/commands/export.py
@@ -0,0 +1,33 @@
+from django.core.management import BaseCommand
+import os
+
+from bitorama.models import *
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Export Posts into files."
+
+    def handle(self, *args, **options):
+        print("This command will export all the posts in your database.")
+        folder = './posts'
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+
+        posts = Post.objects.all()
+        for post in posts:
+            with open(folder + '/' + '%s.post' % (post.title), 'w') as my_file:
+                print('[TITLE]:\n' + post.title, file=my_file)
+                print('\n[DESCRIPTION]:\n' + post.description, file=my_file)
+                print('\n[POST]:\n' + post.post, file=my_file)
+                print('\n[AUTHOR]:\n' + post.author, file=my_file)
+                print('\n[NUMBER_OF_VIEWS]:\n' +
+                      str(post.number_of_views), file=my_file)
+                print('\n[NUMBER_OF_LIKES]:\n' +
+                      str(post.number_of_likes), file=my_file)
+                print('\n[NUMBER_OF_COMMENTS]:\n' +
+                      str(post.number_of_comments), file=my_file)
+                print('\n[DATE_CREATED]:\n' +
+                      str(post.date_created), file=my_file)
+            print('%s.post' % (post.title), ' OK.')
+        print('Done.')
